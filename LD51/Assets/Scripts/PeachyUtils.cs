9a6f7b4e004aaca47c444474a911e423c4425d82
Vasiliy Chalov <100310423+peachyhead@users.noreply.github.com>
2022-10-01 17:31:47 +0700

    Add character controller

--- LD51/Assets/Scripts/PeachyUtils.cs
+++ LD51/Assets/Scripts/PeachyUtils.cs
@@ -1,14 +1,4 @@
-using System.Collections;
-using UnityEngine;
-
 public static class PeachyUtils
 {
-    public static IEnumerator FadeIn(float alpha)
-    {
-        while (alpha > 0)
-        {
-            alpha = Mathf.MoveTowards(alpha, 0, 2 * Time.deltaTime);
-            yield return null;
-        }
-    }
+    
 }
