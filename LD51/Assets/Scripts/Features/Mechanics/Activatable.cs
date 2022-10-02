4e2c35dec5e44caee538832d69b373ee2459cff6
vertoker <vertogpro@gmail.com>
2022-10-02 10:20:13 +0500

    добавил дверь

---
+++ LD51/Assets/Scripts/Features/Mechanics/Activatable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public class Activatable : MonoBehaviour
+    {
+        public virtual void Activate()
+        {
+
+        }
+        public virtual void Deactivate()
+        {
+
+        }
+    }
+}
