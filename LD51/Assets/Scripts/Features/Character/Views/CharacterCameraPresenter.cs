1948294065fbded4efcfb95598f8a08f5d4cc0d3
Vasiliy Chalov <100310423+peachyhead@users.noreply.github.com>
2022-10-02 16:45:39 +0700

    Revert "Merge pull request #2 from peachyband/feature/gameplay-mechanics"
    This reverts commit ae0df674897bfbb33848e0b0c42e3d3c2d92987e, reversing
    changes made to 1d0f2ddb44adab349f752d288928a1e817c420a1.

---
+++ LD51/Assets/Scripts/Features/Character/Views/CharacterCameraPresenter.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Features.Character.Views
+{
+    public class CharacterCameraPresenter : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        
+        public void ChangeFOV(float requiredFOV)
+        {
+            _camera.DOFieldOfView(requiredFOV, 0.25f);
+        }
+    }
+}
---
+++ LD51/Assets/Scripts/PeachyUtils.cs
@@ -0,0 +1,6 @@
+using Cysharp.Threading.Tasks;
+
+public static class PeachyUtils
+{
+    
+}
