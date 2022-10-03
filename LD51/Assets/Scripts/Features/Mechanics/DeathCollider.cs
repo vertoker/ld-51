9960274c614ceb01ba5aa6f843b050e62a707e82
vertoker <vertogpro@gmail.com>
2022-10-03 18:22:31 +0500

    add level 9

--- LD51/Assets/Scripts/Features/Mechanics/DeathCollider.cs
+++ LD51/Assets/Scripts/Features/Mechanics/DeathCollider.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using Features.Core;
 
 namespace Mechanics
 {
     public class DeathCollider : MonoBehaviour
     {
+        [Inject]
+        private CoreEvents events;
+        
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.collider.CompareTag("Player"))
             {
-                Debug.Log("Death");
+                events.OnGameOver?.Invoke();
             }
         }
     }
